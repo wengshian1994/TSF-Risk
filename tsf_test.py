a99added00f1900b127ff55981c26147086faebf
wengshian1994 <wengshih@andrew.cmu.edu>
2018-05-26 23:24:49 -0400

    c

--- tsf_test.py
+++ tsf_test.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from scipy.stats import norm
 
+import math
 import numpy as np
 import pandas as pd
 import quandl
+import matplotlib.pyplot as plt
 
 def make_returns(data):
     
@@ -10,6 +13,19 @@ def make_returns(data):
         data.iloc[i-1] = data.iloc[i]/data.iloc[i-1] - 1
     data = data.drop(len(data)-1,0)
     return data
+
+def make_arrays(stock_names, portfolio_dict):
+    lots = list()
+    sectors = list()
+    p_weights = list()
+    for i in range(len(stock_names)):
+        stock = stock_names[i]
+        lot , industry, weight = portfolio_dict[stock]
+        lots.append(lot)
+        sectors.append(industry)
+        p_weights.append(weight)
+    return lots, sectors, p_weights
+    
 
 if __name__ == "__main__":
     # read respective files
@@ -41,24 +57,48 @@ if __name__ == "__main__":
     lots = list(positions["Quantity"])
     sector = list(positions["Industry"])
     weights = list(positions["P_Weight"])
-    stock_names = list(three_year_cov)
+    stock_names_three_year = list(three_year_cov)
+    stock_names_one_year = list(one_year_cov)
+    stock_names_three_month = list(three_month_cov)
     portfolio_dict = {}
     for i in range(len(names)):
         portfolio_dict[names[i]] = lots[i], sector[i], weights[i]
     
     #want to order the arrays for cov matrix
-    lots = list()
-    sectors = list()
-    p_weights = list()
-    for i in range(len(stock_names)):
-        stock = stock_names[i]
-        lot , industry, weight = portfolio_dict[stock]
-        lots.append(lot)
-        sectors.append(industry)
-        p_weights.append(weight)
+    three_year_lots, three_year_sectors, three_year_p_weights = make_arrays(stock_names_three_year,portfolio_dict)
+    one_year_lots, one_year_sectors, one_year_p_weights = make_arrays(stock_names_one_year,portfolio_dict)
+    three_month_lots, three_month_sectors, three_month_p_weights = make_arrays(stock_names_three_month,portfolio_dict)
+    #rescale weights (can remove this later on)
+    three_year_p_weights = np.array(three_year_p_weights)/sum(three_year_p_weights)
+    one_year_p_weights = np.array(one_year_p_weights)/sum(one_year_p_weights)
+    three_month_p_weights = np.array(three_month_p_weights)/sum(three_month_p_weights)
     
-        
+    # 4. Get respective means and sigmas
+    three_year_mu = (1+np.dot(three_year_p_weights,three_year_mean))**5 -1
+    three_year_sigma = math.sqrt(np.dot(np.dot(three_year_p_weights,three_year_cov).transpose(),three_year_p_weights))*math.sqrt(5)
+    
+    one_year_mu = (1+np.dot(one_year_p_weights,one_year_mean))**5 - 1
+    one_year_sigma = math.sqrt(np.dot(np.dot(one_year_p_weights,one_year_cov).transpose(),one_year_p_weights))*math.sqrt(5)
+    
+    three_month_mu = (1+np.dot(three_month_p_weights,three_month_mean))**5 - 1
+    three_month_sigma = math.sqrt(np.dot(np.dot(three_month_p_weights,three_month_cov).transpose(),three_month_p_weights))*math.sqrt(5)
     
     
-
-#date = { 'gte': str(year-1) + "-" + str(month) + "-" + str(day), 'lte': str(year) + "-" + str(month) + "-" + str(day)}
+    # 5. Calculate VaR
+    ninety_five_VaR_Three_Year = norm.ppf(0.05,loc=three_year_mu,scale=three_year_sigma)
+    ninety_nine_VaR_Three_Year = norm.ppf(0.01,loc=three_year_mu,scale=three_year_sigma)
+    
+    ninety_five_VaR_One_Year = norm.ppf(0.05,loc=one_year_mu,scale=one_year_sigma)
+    ninety_nine_VaR_One_Year = norm.ppf(0.01,loc=one_year_mu,scale=one_year_sigma)
+    
+    ninety_five_VaR_Three_Month = norm.ppf(0.05,loc=three_month_mu,scale=three_month_sigma)
+    ninety_five_VaR_Three_Month = norm.ppf(0.01,loc=three_month_mu,scale=three_month_sigma)
+    VaR_array = [ninety_five_VaR_Three_Year,ninety_nine_VaR_Three_Year,ninety_five_VaR_One_Year,ninety_nine_VaR_One_Year,ninety_five_VaR_Three_Month,ninety_five_VaR_Three_Month]
+    
+    #save dataframe into excel sheet
+    VaR_dataframe = pd.DataFrame()
+    VaR_dataframe["VaR"] = VaR_array
+    VaR_dataframe.rename(index={0:'95 % VaR (Three Years)',1:'99 % VaR (Three Years)',2:'95 % VaR (One Year)',\
+                                3:'99 % VaR (One Year)',4:'95 % VaR (Three Months)',5:'99 % VaR (Three Months)'}, inplace=True)
+    VaR_dataframe.to_csv("VaR_Numbers", sep='\t')
+    
